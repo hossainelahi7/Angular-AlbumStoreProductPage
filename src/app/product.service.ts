7882939cfdab439f8def173cb4ed29a8975563d9
Hossain Elahi <hossain.elahi@live.com>
2021-03-10 20:58:26 -0500

    complete part5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { AlbumStorePage } from '../../e2e/app.po';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -13,10 +13,10 @@ export class ProductService {
     this.getAlbum(1).subscribe();
    }
 
-   getAlbum(id: number){
+   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
     .map((response) =>
-      response.json()
+      <Album>response.json()
     )
     }
 
